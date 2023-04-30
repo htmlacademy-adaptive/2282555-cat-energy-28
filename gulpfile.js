9618e2182c6f935cef610aa6af1ff6200525b40b
Eva Miklyaeva <miklyayeva09@mail.ru>
2023-05-01 00:40:15 +0300

    Исправляет секцию Программы питания

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 import gulp from 'gulp';
 import browser from 'browser-sync';
 import plumber from 'gulp-plumber';
-import data from './source/data.json' assert { type: 'json'};
+import data from './source/data.json'assert {type: 'json'};
 import twig from 'gulp-twig';
 import htmlmin from 'gulp-htmlmin';
-import { htmlValidator } from 'gulp-w3c-html-validator';
+import {
+	htmlValidator
+} from 'gulp-w3c-html-validator';
 import bemlinter from 'gulp-html-bemlinter';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
-import { stacksvg } from 'gulp-stacksvg';
+import {
+	stacksvg
+} from 'gulp-stacksvg';
 import svgo from 'gulp-svgmin';
 import postcss from 'gulp-postcss';
 import postUrl from 'postcss-url';
@@ -16,35 +20,47 @@ import autoprefixer from 'autoprefixer';
 import csso from 'postcss-csso';
 import terser from 'gulp-terser';
 import squoosh from 'gulp-libsquoosh';
-import { deleteAsync } from 'del';
+import {
+	deleteAsync
+} from 'del';
 import gulpIf from 'gulp-if';
 
-const { src, dest, watch, series, parallel } = gulp;
+const {
+	src,
+	dest,
+	watch,
+	series,
+	parallel
+} = gulp;
 const sass = gulpSass(dartSass);
 
 data.isDevelopment = true;
 
-export function processMarkup () {
+export function processMarkup() {
 	return src('./source/*.html')
 		.pipe(twig({
 			data: data
 		}))
-		.pipe(htmlmin({ collapseWhitespace: !data.isDevelopment }))
+		.pipe(htmlmin({
+			collapseWhitespace: !data.isDevelopment
+		}))
 		.pipe(dest('./build'))
 }
 
-export function validateMarkup () {
+export function validateMarkup() {
 	return src('./build/*.html')
 		.pipe(htmlValidator.analyzer())
-		.pipe(htmlValidator.reporter({ throwErrors: true }));
+		.pipe(htmlValidator.reporter({
+			throwErrors: true
+		}));
 }
 
-export function lintBem () {
+export function lintBem() {
 	return src('./build/*.html')
 		.pipe(bemlinter());
 }
 
-export function processStyles () {
+export function processStyles() {
 	const sassOptions = {
 		functions: {
 			'getbreakpoint($bp)': (bp) => new dartSass.types.Number(data.viewports[bp.getValue()]),
@@ -53,78 +69,90 @@ export function processStyles () {
 			'getviewports($name)': function (name) {
 				let vps = data.images[name.getValue()].sizes.map((size) => size.viewport);
 				let viewports = new dartSass.types.List(vps.length);
-				vps.reverse().forEach((vp, i) => { viewports.setValue(i, new dartSass.types.String(vp)) });
+				vps.reverse().forEach((vp, i) => {
+					viewports.setValue(i, new dartSass.types.String(vp))
+				});
 				return viewports;
 			}
 		}
 	}
 
-	return src('./source/sass/*.scss', { sourcemaps: data.isDevelopment })
+	return src('./source/sass/*.scss', {
+			sourcemaps: data.isDevelopment
+		})
 		.pipe(plumber())
 		.pipe(sass(sassOptions).on('error', sass.logError))
 		.pipe(postcss([
-			postUrl({ assetsPath: '../' }),
+			postUrl({
+				assetsPath: '../'
+			}),
 			autoprefixer(),
 			csso()
 		]))
-		.pipe(dest('./build/css', { sourcemaps: data.isDevelopment }))
+		.pipe(dest('./build/css', {
+			sourcemaps: data.isDevelopment
+		}))
 		.pipe(browser.stream());
 }
 
-export function processScripts () {
+export function processScripts() {
 	return src('./source/js/*.js')
 		.pipe(terser())
 		.pipe(dest('./build/js'))
 		.pipe(browser.stream());
 }
 
-export function optimizeImages () {
+export function optimizeImages() {
 	return src('./source/img/**/*.{png,jpg}')
 		.pipe(gulpIf(!data.isDevelopment, squoosh()))
 		.pipe(dest('build/img'))
 }
 
-export function createWebp (done) {
+export function createWebp(done) {
 	if (!data.isDevelopment) {
 		return src('./source/img/**/*.{jpg,png}')
-			.pipe(squoosh({ webp: {} }))
+			.pipe(squoosh({
+				webp: {}
+			}))
 			.pipe(dest('./build/img'))
 	} else {
 		done()
 	}
 }
 
-export function createAvif (done) {
+export function createAvif(done) {
 	if (!data.isDevelopment) {
 		return src('./source/img/**/*.{jpg,png}')
-			.pipe(squoosh({ avif: {} }))
+			.pipe(squoosh({
+				avif: {}
+			}))
 			.pipe(dest('./build/img'))
 	} else {
 		done()
 	}
 }
 
-export function createStack () {
+export function createStack() {
 	return src('./source/icons/**/*.svg')
 		.pipe(svgo())
 		.pipe(stacksvg())
 		.pipe(dest('./build/icons'));
 }
 
-export function copyAssets () {
+export function copyAssets() {
 	return src([
-		'./source/fonts/*.{woff2,woff}',
-		'./source/*.ico',
-		'./source/img/**/*.svg',
-		'./source/favicons/*',
-		'./source/*.webmanifest'
-	], {
-		base: './source'
-	})
+			'./source/fonts/*.{woff2,woff}',
+			'./source/*.ico',
+			'./source/img/**/*.svg',
+			'./source/favicons/*',
+			'./source/*.webmanifest'
+		], {
+			base: './source'
+		})
 		.pipe(dest('./build'))
 }
 
-export function removeBuild () {
+export function removeBuild() {
 	return deleteAsync('./build');
 };
 
@@ -136,23 +164,24 @@ export function startServer(done) {
 		cors: true,
 		notify: false,
 		ui: false,
+		browser: "firefox"
 	});
 	done();
 }
 
-function reloadServer (done) {
+function reloadServer(done) {
 	browser.reload();
 	done();
 }
 
-function watchFiles () {
+function watchFiles() {
 	watch('./source/sass/**/*.scss', series(processStyles));
 	watch('./source/js/*.js', series(processScripts, reloadServer));
 	watch(['./source/**/*.{html,twig}', './source/**/_data.js'], series(processMarkup, reloadServer));
 	watch('./source/icons/**/*.svg', series(createStack, reloadServer));
 }
 
-export function compileProject (done) {
+export function compileProject(done) {
 	parallel(
 		processStyles,
 		processMarkup,
@@ -165,7 +194,7 @@ export function compileProject (done) {
 	)(done);
 }
 
-export function buildProd (done) {
+export function buildProd(done) {
 	data.isDevelopment = false;
 	series(
 		removeBuild,
@@ -173,7 +202,7 @@ export function buildProd (done) {
 	)(done);
 }
 
-export function runDev (done) {
+export function runDev(done) {
 	series(
 		removeBuild,
 		compileProject,
