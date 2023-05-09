0391ba2f113d6f2d0e33e5cb5c0844b302fae35f
Eva Miklyaeva <miklyayeva09@mail.ru>
2023-05-09 19:02:56 +0300

    Мелкие правки

--- source/js/navigation-script.js
+++ source/js/navigation-script.js
@@ -2,10 +2,9 @@ let navMain = document.querySelector('.navigation');
 let navToggle = document.querySelector('.navigation__toggle');
 
 navMain.classList.remove('navigation--nojs');
-navMain.classList.toggle('navigation--opened');
-navMain.classList.toggle('navigation--closed');
+navMain.classList.replace('navigation--opened', 'navigation--closed');
 
 navToggle.addEventListener('click', function () {
-  navMain.classList.toggle('navigation--closed');
-  navMain.classList.toggle('navigation--opened');
+	navMain.classList.toggle('navigation--closed');
+	navMain.classList.toggle('navigation--opened');
 });
